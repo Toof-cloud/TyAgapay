32cdc3f13c884465e4923b6561dcdc4ba95ec158
pascualtyrone <tyronepascual099@gmail.com>
2025-06-13 22:10:43 +0800

    ALL FIXED

--- src/model/Citizen.java
+++ src/model/Citizen.java
@@ -1,4 +1,5 @@
 package model;
+
 import javafx.beans.property.SimpleStringProperty;
 
 public class Citizen {
@@ -18,8 +19,45 @@ public class Citizen {
     private SimpleStringProperty country_name;
     private SimpleStringProperty status;
 
+    // 14-parameter constructor (no status)
+    public Citizen(
+        String citizen_id,
+        String citizenlastname,
+        String citizenfirstname,
+        String contactNum,
+        String marital_status,
+        String household_no,
+        String valid_id_type,
+        String valid_id_code,
+        String house_no,
+        String barangay_name,
+        String city_name,
+        String zip,
+        String region_name,
+        String country_name
+    ) {
+        this(citizen_id, citizenlastname, citizenfirstname, contactNum, marital_status, household_no,
+            valid_id_type, valid_id_code, house_no, barangay_name, city_name, zip, region_name, country_name, "");
+    }
 
-    public Citizen(String citizen_id, String citizenlastname, String citizenfirstname, String contactNum, String marital_status, String household_no, String valid_id_type, String valid_id_code, String house_no, String barangay_name, String city_name, String zip, String region_name, String country_name, String status) {
+    // 15-parameter constructor (with status)
+    public Citizen(
+        String citizen_id,
+        String citizenlastname,
+        String citizenfirstname,
+        String contactNum,
+        String marital_status,
+        String household_no,
+        String valid_id_type,
+        String valid_id_code,
+        String house_no,
+        String barangay_name,
+        String city_name,
+        String zip,
+        String region_name,
+        String country_name,
+        String status
+    ) {
         this.citizen_id = new SimpleStringProperty(citizen_id);
         this.citizenlastname = new SimpleStringProperty(citizenlastname);
         this.citizenfirstname = new SimpleStringProperty(citizenfirstname);
@@ -36,48 +74,63 @@ public class Citizen {
         this.country_name = new SimpleStringProperty(country_name);
         this.status = new SimpleStringProperty(status);
     }
+
     public String getCitizenID() {
         return citizen_id.get();
     }
+
     public String getCitizenLastname() {
         return citizenlastname.get();
     }
+
     public String getCitizenFirstname() {
         return citizenfirstname.get();
     }
+
     public String getContactNum() {
         return contactNum.get();
     }
+
     public String getMaritalStatus() {
         return marital_status.get();
     }
+
     public String getHouseholdNo() {
         return household_no.get();
     }
+
     public String getValidIdType() {
         return valid_id_type.get();
     }
+
     public String getValidIdCode() {
         return valid_id_code.get();
     }
+
     public String getHouseNo() {
         return house_no.get();
     }
+
     public String getBarangayName() {
         return barangay_name.get();
     }
+
     public String getCityName() {
         return city_name.get();
     }
+
     public String getZip() {
         return zip.get();
     }
+
     public String getRegionName() {
         return region_name.get();
     }
+
     public String getCountryName() {
         return country_name.get();
     }
+
     public String getStatus() {
         return status.get();
     }
